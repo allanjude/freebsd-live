7128b805db00b014fb87b8689eaaa75b49f74bf6
Allan Jude <allanjude@freebsd.org>
2019-09-20 03:52:17 -0400

    Check in the initial work

---
+++ index.php
@@ -0,0 +1,205 @@
+<?php
+require_once("scaleengine.php");
+
+$arr_streams = json_decode(file_get_contents("streams.json"), true);
+$arr_secrets = json_decode(file_get_contents("secrets.json"), true);
+
+$arr_uri = explode('/', $_SERVER['REQUEST_URI']);
+foreach ($arr_uri as $key => $val) {
+	if ($val === "") {
+		unset($arr_uri[$key]);
+	}
+}
+$arr_uri = array_values($arr_uri);
+$selected_conf = "";
+if (isset($arr_uri[0])) {
+	$selected_conf = $arr_uri[0];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" class="js csstransforms3d">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="description" content="">
+	<link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon" />
+	<link rel="icon" href="/images/favicon.png" type="image/x-icon" />
+	<title>FreeBSD Live Streaming</title>
+	
+	<link href="/css/nucleus.css" rel="stylesheet">
+	<link href="/css/font-awesome.min.css" rel="stylesheet">
+	<link href="/css/hybrid.css" rel="stylesheet">
+	<link href="/css/featherlight.min.css" rel="stylesheet">
+	<link href="/css/perfect-scrollbar.min.css" rel="stylesheet">
+	<link href="/css/auto-complete.css" rel="stylesheet">
+	<link href="/css/theme.css" rel="stylesheet">
+	<link href="/css/hugo-theme.css" rel="stylesheet">
+	<link href="/css/theme-red.css" rel="stylesheet">
+	<script src="/js/jquery-2.x.min.js"></script>
+	<style type="text/css">
+		:root #header + #content > #left > #rlblock_left
+		{ 
+			display:none !important;
+		}
+	</style>
+	<script type="text/javascript">
+	    
+		var baseurl = '\/';
+	    
+	</script>
+    
+	<link href="/css/papers-custom.css" rel="stylesheet">
+	<link href="/live-custom.css" rel="stylesheet">
+
+  </head>
+  <body class="" data-url="<?=$_SERVER['REQUEST_URI']?>">
+	<nav id="sidebar" class="">
+		<div id="header-wrapper">
+			<div id="header">
+				<a id="logo" href="https://live.freebsd.org/">
+					<img src="https://www.freebsd.org/logo/logo-full.png">
+				</a>
+			</div>
+		</div>
+		<div class="highlightable">
+			<ul class="topics">
+
+			<?php foreach ($arr_streams as $conf => $arr_conf): ?>
+
+			<li data-nav-id="/<?=$conf?>/" title="<?=$conf?>" class="dd-item parent">
+				<a href="/<?=$conf?>/">
+
+					<?php if ($selected_conf == $conf):?>
+					<i class="fa fa-angle-double-down"></i>
+					<?php else:?>
+					<i class="fa fa-angle-double-right"></i>
+					<?php endif;?>
+
+					<?=$conf?>
+
+				</a>
+				<ul class="ancestor-range-pages">
+
+				<?php
+				$arr_all_streams = array();
+				switch ($arr_conf['type']) {
+					case "scaleengine":
+						$error = NULL;
+						$secrets = $arr_secrets[$conf];
+						$keys = "{$secrets['cdn_id']}:{$secrets['api_key']}";
+						$arr_all_streams = seapi($keys, "stream_metadata", "GET", NULL, $error);
+						break;
+					case "youtube":
+						$arr_all_streams = $arr_conf['urls'];
+						break;
+					case "link":
+						$arr_all_streams = $arr_conf['urls'];
+						break;
+				}
+				if ($arr_all_streams):
+					foreach ($arr_all_streams as $stream => $arr_details):?>
+
+					<li data-nav-id="/<?=$conf?>/<?=$stream?>/" title="<?=$conf?> - <?=$stream?>" class="dd-item ">
+						<a href="/<?=$conf?>/<?=$stream?>/">
+							<i class="fa fa-angle-right papers-menu-item"></i>
+							 <?=$stream?>
+						</a>
+					</li>
+
+					<?php endforeach; /* stream */ ?>
+				<?php endif; ?>
+
+				</ul>
+			</li>
+
+			<?php endforeach; /* conference */ ?>
+
+			</ul>
+			<section id="footer">
+			</section>
+		</div>
+	</nav>
+
+
+        <section id="body">
+          <div class="padding highlightable">
+		<div id="body-inner">
+
+<?php
+if (count($arr_uri) == 0) {
+	/* Home Page */
+?>
+
+			<p>This page contains the live streams for many FreeBSD related events</p>
+			<p>Streams may only be available while the events is in progress.</p>
+
+<?php
+} elseif (count($arr_uri) == 1) {
+	/* Conference Page */
+?>
+
+			<p>Select a stream from the &quot;<?=$arr_config[$selected_conf]['description']?>&quot; from the menu on the left.</p>
+
+<?php
+} else {
+	/* Stream Page */
+	$stream = $arr_uri[2];
+	$config = $arr_config[$selected_conf];
+
+	switch ($config['type']) {
+		case "scaleengine":
+			$secrets = $arr_secrets[$selected_conf];
+			$keys = "{$secrets['cdn_id']}:{$secrets['api_key']}";
+			$arr_details = seapi($keys, "stream_metadata", "GET", $stream, $error);
+			print_r($arr_details);
+?>
+
+			<h1><?=$conference?> - <?=$stream?></h1>
+
+			<script type="text/javascript" src="//eurobsdcon-embed.secdn.net/clappr/0.3.8/clappr.min.js"></script>
+			<script type="text/javascript" src="//eurobsdcon-embed.secdn.net/clappr/0.3.8/level-selector.min.js"></script>
+			
+			<div id="se_video_5"></div>
+			
+			<script type="text/javascript">
+			var player = new Clappr.Player({
+			       source: 'https://eurobsdcon-hls.secdn.net/eurobsdcon-live/play/room5.smil/playlist.m3u8',
+			       parentId: "#se_video_5",
+			       autoPlay: true ,
+			       poster: 'https://eurobsdcon-hls.secdn.net/eurobsdcon-live/play/room5/thumbnail.jpg',
+			       width: '720',
+			       height: '400',
+			       plugins: {core: [LevelSelector], playback: []},
+			});
+			</script>
+			
+			<p><a href="rtmp://eurobsdcon-vsn.secdn.net/eurobsdcon-live/play/room5">RTMP Link (Lower Latency)</a></p>
+
+<?php
+			break;
+
+		case "youtube":
+			break;
+			
+		case "link":
+			break;
+	}
+}
+?>
+
+		</div>
+	  </div>
+	</section>
+
+	<script src="/js/perfect-scrollbar.min.js"></script>
+	<script src="/js/perfect-scrollbar.jquery.min.js"></script>
+	<script src="/js/jquery.sticky.js"></script>
+	<script src="/js/featherlight.min.js"></script>
+	<script src="/js/html5shiv-printshiv.min.js"></script>
+	<script src="/js/modernizr.custom.71422.js"></script>
+	<script src="/js/learn.js"></script>
+	<script src="/js/hugo-learn.js"></script>
+  </body>
+</html>
+
---
+++ scaleengine.php
@@ -0,0 +1,51 @@
+<?php
+
+function seapi($credentials, $endpoint, $method = "GET", $body = "", &$error = NULL, $timeout=180)
+{
+	$request = "{invalid}";
+	$ch = curl_init();
+
+	if (strtoupper($method) == "GET")
+	{
+		$request = "https://api.scaleengine.net/dev/v2/{$endpoint}/?{$body}";
+		curl_setopt($ch, CURLOPT_URL, $request);
+	}
+	else
+	{
+		$request = "{https://api.scaleengine.net/dev/v2/{$endpoint}/";
+		curl_setopt($ch, CURLOPT_URL, $request);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+	}
+	curl_setopt($ch, CURLOPT_PROXY, 'proxy:3128');
+	curl_setopt($ch, CURLOPT_USERPWD, $credentials);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+	
+	$response = curl_exec($ch); //Execute the API Call
+	if (!$response)
+	{
+		$error = 'Failed to connect to ScaleEngine API: '.curl_error($ch);
+		return (false);
+	}
+	$arr_response = json_decode($response, true);
+
+	$arr_info = curl_getinfo($ch);
+	if ($arr_info['http_code'] > 299)
+	{
+		$error = "API Error: {$response}";
+		return (false);
+	}
+
+	if (isset($arr_response['data']))
+	{
+		$error = null;
+		return $arr_response['data'];
+	}
+	else
+	{
+		$error = "Unknown API Error: {$response}";
+		return (false);
+	}
+}
